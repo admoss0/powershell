4eed98f4a6be802989bae176ac0ee4f8be298ee2
admoss0 <admoss0@gmail.com>
2014-08-23 12:45:26 +1000

    add printers.ps1

---
+++ printers.ps1
@@ -0,0 +1,5 @@
+﻿$target = Read-Host -Prompt "Printer Info`r`nEnter server to scan"
+Get-Printer -ComputerName $target | Export-Csv c:\temp\$target.csv
+
+out-host -InputObject "results are in c:\temp\$target.csv"
+
